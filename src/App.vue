f0b74e1d94db0a8f367574e8459589f3d492fffe
ppyyll00 <qkrdpfla032@naver.com>
2023-07-19 11:50:39 +0900

    todo list

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,57 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form
+    class="d-flex"
+    @submit.prevent="onSubmit" 
+    >
+      <div class="flex-grow-1 mr-2"> 
+        <input
+          class="form-control "
+          type="text"
+          v-model="todo"
+          placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button
+        class="btn btn-primary"
+        type="submit"
+        >
+        Add
+        </button>
+      </div>      
+    </form>
+    {{ todos }}
+  </div>
+  
+  
+</template>
+  
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const todo = ref(' ');
+    const todos = ref([])
+
+    const onSubmit = ( ) => {
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
+    };
+
+    return {
+      todo,
+      onSubmit,
+      todos,
+    };
+  }
+}
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="dddddd" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
