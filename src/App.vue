4be877e3a8bd6b6a05d4692eadcfbee741869d8b
ppyyll00 <qkrdpfla032@naver.com>
2023-07-26 12:14:13 +0900

    props 사용과 사용시 주의사항

--- src/App.vue
+++ src/App.vue
@@ -6,47 +6,23 @@
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
-    <div 
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
-    >
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input 
-            class="form-check-input" 
-            type="checkbox"
-            v-model="todo.completed"
-          >
-          <label 
-            class="form-check-label"
-            :class="{ todo: todo.completed }"
-          >
-            {{ todo.subject }}
-          </label>
-        </div>
-        <div>
-          <button 
-            class="btn btn-danger btn-sm"
-            @click="deleteTodo(index)"
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
+    <TodosForm :todos="todos" @toggle-todo="toggleTodo"/> <!--props를 이용하여 부모에 있는 "todos"를 todos 이름으로 자식컴포넌트(TodosForm) 에 전달-->
+    <!-- 자식한테 'toggle-todo'로 받은 index를 toggleTodo라는 함수에 적용시킨다. -->
+    
   </div>
 </template>
 <script>
 import { ref } from 'vue'
 import TodoSimpleForm from '@/components/TodoSimpleForm.vue'
+import TodosForm from '@/components/TodosForm.vue'
 
 export default {
   components: {
-    TodoSimpleForm
+    TodoSimpleForm,
+    TodosForm
   },
   setup() {
-    const todos = ref([])
+    const todos = ref([]) /*[] = array = 배열*/
 
     const addTodo = (todo) => {
       todos.value.push(todo);
@@ -56,10 +32,16 @@ export default {
       todos.value.splice(index,1)
     }
 
+    const toggleTodo = (index) => {
+      todos.value[index].completed = !todos.value[index].completed  /* todos를 TodosForm으로 받아서 브이포로 사용하고 그 인덱스를 다시 부모한테 전달했음 */ 
+      /* 인풋의 인덱스가 트루면 펄스로, 펄스면 트루로 변경, (마우스 클릭시 적용) */
+    }
+
     return{
       todos,
       addTodo,
       deleteTodo,
+      toggleTodo,
     }
   }
 }
--- src/components/TodosForm.vue
+++ src/components/TodosForm.vue
@@ -1,17 +1,21 @@
 <template>
-  <div
-      v-for="(todo,index) in todos" 
-      key="todo.id"
+  <div 
+      v-for="(todo, index) in todos"
+      :key="todo.id"
       class="card mt-2"
     >
-      <div class="card-body d-flex align-items-center">
-        <div clsss="form-check flex-grow-1">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input 
             class="form-check-input" 
             type="checkbox"
-            v-model="todos.completed"
+            :value="todo.completed"
+            @change="toggleTodo(index)"            
+          ><!-- @change = 인풋값이 변경될때마다 toggleTodo라는 함수를 실행시킴, index는 todos fmf 브이포 로 돌릴떄 나오는 index다 -->
+          <label 
+            class="form-check-label"
+            :class="{ todo: todo.completed }"
           >
-          <label class="form-check-label">
             {{ todo.subject }}
           </label>
         </div>
@@ -20,17 +24,30 @@
             class="btn btn-danger btn-sm"
             @click="deleteTodo(index)"
           >
-            delete
+            Delete
           </button>
         </div>
       </div>
     </div>
 </template>
 <script>
-export default defineComponent({
-  setup() {
-    
+export default {
+  // props: ['todos'] /*부모컴포넌트에 todos를 받기 위해 삽입*/
+  props: {
+    todos: {
+      type: Array, /*App에서 todos를 array로 정의했기 때문에 배열로 받아야 한다.*/
+      required: true, /*todos를 배열로 해줄거기 때문에 true로 설정 */
+    }
   },
-})
+  setup(props, context) {
+    const toggleTodo = (index) => {
+      context.emit('toggle-todo', index) /*인풋값이 변경될때 input값을 'toggle-todo'이름으로 부모한테 넘기겠다. */
+    }
+
+    return {
+      toggleTodo,
+    }
+  }
+}
 </script>
 <style></style>
